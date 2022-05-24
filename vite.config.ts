d334e2ca40a446e15ebfafafc974b00645df2b00
high1 <high1@mailfence.com>
2022-05-24 23:04:50 +0200

    Updated voby
    - updated voby - MDX math works!

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,16 @@
 import { defineConfig } from 'vite';
 import mdx from '@mdx-js/rollup';
-import voby from 'voby/vite';
+import voby from 'voby-vite';
 import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import remarkMath from 'remark-math';
-import rehypeMathjax from 'rehype-mathjax/svg.js';
-// import rehypeMathjaxChtml from "rehype-mathjax/chtml.js";
+import rehypeMathjax from 'rehype-mathjax';
 
 export default defineConfig({
   plugins: [
-    mdx({ jsxImportSource: 'voby', remarkPlugins: [remarkMath], rehypePlugins: [
-      rehypeMathjax,
-      // [rehypeMathjaxChtml, {
-      // 	chtml: {
-      // 		fontURL: "https://cdn.jsdelivr.net/npm/mathjax@3/es5/output/chtml/fonts/woff-v2",
-      // 	},
-      // }],
-    ] }),
+    mdx({ jsxImportSource: 'voby', remarkPlugins: [remarkMath], rehypePlugins: [rehypeMathjax] }),
     voby(),
     uno(),
     tsconfigPaths(),
   ],
 });
-``
