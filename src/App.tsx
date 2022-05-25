120191338c5f0d8dfaede8dcd870ee0585556245
high1 <high1@mailfence.com>
2022-05-25 13:32:40 +0200

    feat(css): full height and background

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import MathJax from 'Mathjax.mdx';
 
 export const App = (): JSX.Element => (
-  <div class="flex flex-col items-center justify-center">
+  <div class="flex flex-col items-center justify-center h-full bg-amber-50">
     <MathJax components={{ h1: (props) => <h1 class="text-sky-600" {...props} /> }} />;
   </div>
 );
