df2ca52d1e95ce64e39e11a05d00edab8b4ccbbd
high1 <high1@mailfence.com>
2022-06-06 00:10:50 +0200

    chore: updated packages
    - updated packages
    - used component override, instead of html in mdx

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import 'virtual:uno.css';
-import { render } from 'voby';
-import { App } from 'App';
+import "virtual:uno.css";
+import { render } from "voby";
+import { App } from "App";
 
-render(<App />, document.querySelector<HTMLDivElement>('#root'));
+render(<App />, document.querySelector("#root"));
