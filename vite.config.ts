ceb6d905cd7caea077ba8de32760d2235fd6b844
high1 <high1@mailfence.com>
2022-08-12 00:04:17 +0200

    chore: updated packages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import mdx from '@mdx-js/rollup';
-import voby from 'voby-vite';
 import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import remarkMath from 'remark-math';
@@ -8,9 +7,18 @@ import rehypeMathjax from 'rehype-mathjax';
 
 export default defineConfig({
   base: '/voby-mathjax/',
+  build: {
+    commonjsOptions: { include: [] },
+  },
+  esbuild: {
+    jsx: 'automatic',
+    jsxImportSource: 'voby',
+  },
+  optimizeDeps: {
+    disabled: false,
+  },
   plugins: [
     mdx({ jsxImportSource: 'voby', remarkPlugins: [remarkMath], rehypePlugins: [rehypeMathjax] }),
-    voby(),
     uno(),
     tsconfigPaths(),
   ],
